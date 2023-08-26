4b3206afb85992b165b7f8dec0d3aa01e3a017a5
superthomis <superthomis@gmail.com>
2023-08-26 23:36:06 +0200

    passage en php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$HTML = <<<HTML 
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <title>Thomas</title>
+    </head>
+    <body>
+        <h1>Ceci est mon site</h1>
+        <p>et tu peux le voir sur n'importe quel écran</p>
+    </body>
+    </html>
+HTML;
